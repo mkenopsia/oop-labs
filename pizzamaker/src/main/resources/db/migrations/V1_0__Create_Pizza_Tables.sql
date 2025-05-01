1b4605667975d9621fa62cff58ab8435b105a65d
mkenopsia <gxkurro@gmail.com>
2025-05-01 09:07:35 +0700

    update migrations

--- pizzamaker/src/main/resources/db/migrations/V1_0__Create_Pizza_Tables.sql
+++ pizzamaker/src/main/resources/db/migrations/V1_0__Create_Pizza_Tables.sql
@@ -1,71 +1,67 @@
 CREATE SCHEMA IF NOT EXISTS pizza;
 
--- таблица ингредиентов
 CREATE TABLE pizza.t_ingredients
 (
-    id    SERIAL PRIMARY KEY,
-    name  VARCHAR(100)   NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    CONSTRAINT uk_ingredient_name UNIQUE (name)
+    id    serial primary key,
+    name  varchar(100)   not null ,
+    price decimal(10, 2) not null ,
+    constraint uk_ingredient_name unique (name)
 );
 
--- таблица основ для пиццы
 CREATE TABLE pizza.t_pizza_bases
 (
-    id    SERIAL PRIMARY KEY,
-    type  VARCHAR(50)    NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    CONSTRAINT uk_pizza_base_type UNIQUE (type)
+    id    serial primary key,
+    type  varchar(50)    not null ,
+    price decimal(10, 2) not null ,
+    constraint uk_pizza_base_type unique (type)
 );
 
--- таблица бортов
 CREATE TABLE pizza.t_pizza_boards
 (
-    id SERIAL PRIMARY KEY,
-    price DECIMAL(10, 2) NOT NULL
+    id serial primary key ,
+    name varchar(50) not null,
+    price decimal(10, 2) not null
 );
 
--- таблица пицц
 CREATE TABLE pizza.t_pizzas
 (
-    id             SERIAL PRIMARY KEY,
-    name           VARCHAR(100)   NOT NULL,
-    pizza_base_id  INTEGER        NOT NULL,
-    price          DECIMAL(10, 2) NOT NULL,
-    CONSTRAINT fk_pizza_base FOREIGN KEY (pizza_base_id) REFERENCES pizza.t_pizza_bases (id)
+    id             serial primary key ,
+    name           varchar(100)   not null ,
+    pizza_base_id  int        not null ,
+    price          decimal(10, 2) not null ,
+    constraint fk_pizza_base foreign key (pizza_base_id) references pizza.t_pizza_bases (id)
 );
 
--- связующая таблица для ингредиентов пиццы(многие-ко-многим)
+-- связующая таблица для ингредиентов (многие-ко-многим)
 CREATE TABLE pizza.t_pizza_ingredients
 (
-    pizza_id      INTEGER NOT NULL,
-    ingredient_id INTEGER NOT NULL,
-    PRIMARY KEY (pizza_id, ingredient_id),
-    CONSTRAINT fk_pizza_ingredient_pizza FOREIGN KEY (pizza_id) REFERENCES pizza.t_pizzas (id) ON DELETE CASCADE,
-    CONSTRAINT fk_pizza_ingredient_ingredient FOREIGN KEY (ingredient_id) REFERENCES pizza.t_ingredients (id) ON DELETE CASCADE
+    pizza_id      int not null ,
+    ingredient_id int not null ,
+    primary key (pizza_id, ingredient_id),
+    constraint fk_pizza_ingredient_pizza foreign key (pizza_id) references pizza.t_pizzas (id) on delete cascade ,
+    constraint fk_pizza_ingredient_ingredient foreign key (ingredient_id) references pizza.t_ingredients (id) on delete cascade
 );
 
 -- связующая таблица для ингредиентов борта(многие-ко-многим)
 CREATE TABLE pizza.t_pizza_board_ingredients
 (
-    pizza_board_id INTEGER NOT NULL,
-    ingredient_id  INTEGER NOT NULL,
-    PRIMARY KEY (pizza_board_id, ingredient_id),
-    CONSTRAINT fk_board_ingredient_board FOREIGN KEY (pizza_board_id) REFERENCES pizza.t_pizza_boards (id) ON DELETE CASCADE,
-    CONSTRAINT fk_board_ingredient_ingredient FOREIGN KEY (ingredient_id) REFERENCES pizza.t_ingredients (id) ON DELETE CASCADE
+    pizza_board_id int not null ,
+    ingredient_id  int not null ,
+    primary key (pizza_board_id, ingredient_id),
+    constraint fk_board_ingredient_board foreign key (pizza_board_id) references pizza.t_pizza_boards (id) on delete cascade ,
+    constraint fk_board_ingredient_ingredient foreign key (ingredient_id) references pizza.t_ingredients (id) on delete cascade
 );
 
 -- связующая таблица для доступных пицц для бортов(многие-ко-многим)
 CREATE TABLE pizza.t_pizza_board_available_pizzas
 (
-    pizza_board_id INTEGER NOT NULL,
-    pizza_id       INTEGER NOT NULL,
-    PRIMARY KEY (pizza_board_id, pizza_id),
-    CONSTRAINT fk_board_pizza_board FOREIGN KEY (pizza_board_id) REFERENCES pizza.t_pizza_boards (id) ON DELETE CASCADE,
-    CONSTRAINT fk_board_pizza_pizza FOREIGN KEY (pizza_id) REFERENCES pizza.t_pizzas (id) ON DELETE CASCADE
+    pizza_board_id int not null ,
+    pizza_id       int not null ,
+    primary key (pizza_board_id, pizza_id),
+    constraint fk_board_pizza_board foreign key (pizza_board_id) references pizza.t_pizza_boards (id) on delete cascade ,
+    constraint fk_board_pizza_pizza foreign key (pizza_id) references pizza.t_pizzas (id) on delete cascade
 );
 
--- индексы
 CREATE INDEX idx_ingredients_name ON pizza.t_ingredients (name);
 CREATE INDEX idx_pizza_bases_type ON pizza.t_pizza_bases (type);
 CREATE INDEX idx_pizzas_name ON pizza.t_pizzas (name);
--- pizzamaker/src/main/resources/db/migrations/V2_0__Create_Order_Tables.sql
+++ pizzamaker/src/main/resources/db/migrations/V2_0__Create_Order_Tables.sql
@@ -3,10 +3,11 @@ CREATE SCHEMA IF NOT EXISTS ordering;
 CREATE TABLE ordering.t_pizza_order
 (
     id      serial primary key,
-    user_id int         not null,
+    user_id int            not null,
     date    timestamp without time zone,
-    status  varchar(50) not null,
-    comment varchar(500)
+    status  varchar(50)    not null,
+    comment varchar(500),
+    price   decimal(10, 2) not null
 );
 
 CREATE INDEX idx_order_user ON ordering.t_pizza_order (user_id);
@@ -15,12 +16,12 @@ CREATE INDEX idx_order_status ON ordering.t_pizza_order (status);
 CREATE TABLE ordering.t_pizza_for_order
 (
     id       serial primary key,
+    order_id int          not null,
     name     varchar(100) not null,
     size     varchar(10)  not null,
     board_id int          not null,
-    order_id int          not null,
     constraint fk_pizza_order foreign key (order_id) references ordering.t_pizza_order (id) on delete cascade,
-    constraint fk_pizza_board foreign key (board_id) references pizza.t_pizza_boards (id)
+    constraint fk_pizza_board foreign key (board_id) references pizza.t_pizza_boards (id) on delete cascade
 );
 
 CREATE INDEX idx_pizza_for_order_order ON ordering.t_pizza_for_order (order_id);
@@ -31,7 +32,7 @@ CREATE TABLE ordering.t_ingredients_for_pizza_for_order
     pizza_id      int not null,
     ingredient_id int not null,
     constraint fk_pizza_for_order foreign key (pizza_id) references ordering.t_pizza_for_order (id) on delete cascade,
-    constraint fk_ingredient foreign key (ingredient_id) references pizza.t_ingredients (id)
+    constraint fk_ingredient foreign key (ingredient_id) references pizza.t_ingredients (id) on delete cascade
 );
 
 CREATE INDEX idx_ingredients_for_pizza ON ordering.t_ingredients_for_pizza_for_order (pizza_id);
